61ab217a73879efa206a0b07c8b72102b596fb58
AlejandroRujano <125770071+AlejandroRujano@users.noreply.github.com>
2023-04-02 22:25:16 -0400

    Add files via upload

---
+++ Vender2.0/Vender/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Vender
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvVender = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.gBoxNuevoProducto = new System.Windows.Forms.GroupBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gBoxProductos = new System.Windows.Forms.GroupBox();
+            this.lstvProductos = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvVender)).BeginInit();
+            this.gBoxNuevoProducto.SuspendLayout();
+            this.gBoxProductos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgvVender
+            // 
+            this.dtgvVender.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvVender.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Descripcion,
+            this.Cantidad,
+            this.Precio,
+            this.dataGridViewTextBoxColumn1});
+            this.dtgvVender.Location = new System.Drawing.Point(12, 317);
+            this.dtgvVender.Name = "dtgvVender";
+            this.dtgvVender.RowHeadersWidth = 51;
+            this.dtgvVender.RowTemplate.Height = 29;
+            this.dtgvVender.Size = new System.Drawing.Size(696, 180);
+            this.dtgvVender.TabIndex = 0;
+            this.dtgvVender.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvVender_CellClick);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.MinimumWidth = 6;
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Width = 125;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.MinimumWidth = 6;
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Width = 125;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.MinimumWidth = 6;
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.Width = 125;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Precio Total";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(26, 55);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(58, 20);
+            this.lblCodigo.TabIndex = 1;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(119, 52);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(125, 27);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(119, 95);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(125, 27);
+            this.txtDescripcion.TabIndex = 4;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(26, 97);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(87, 20);
+            this.lblDescripcion.TabIndex = 3;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(119, 137);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(125, 27);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(26, 141);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(69, 20);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(119, 180);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(125, 27);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(26, 183);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(50, 20);
+            this.lblPrecio.TabIndex = 7;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(321, 97);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(94, 29);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(321, 133);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(86, 31);
+            this.btnBorrar.TabIndex = 12;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // gBoxNuevoProducto
+            // 
+            this.gBoxNuevoProducto.Controls.Add(this.txtPrecio);
+            this.gBoxNuevoProducto.Controls.Add(this.lblPrecio);
+            this.gBoxNuevoProducto.Controls.Add(this.txtCantidad);
+            this.gBoxNuevoProducto.Controls.Add(this.lblCantidad);
+            this.gBoxNuevoProducto.Controls.Add(this.txtDescripcion);
+            this.gBoxNuevoProducto.Controls.Add(this.lblDescripcion);
+            this.gBoxNuevoProducto.Controls.Add(this.txtCodigo);
+            this.gBoxNuevoProducto.Controls.Add(this.lblCodigo);
+            this.gBoxNuevoProducto.Location = new System.Drawing.Point(25, 78);
+            this.gBoxNuevoProducto.Name = "gBoxNuevoProducto";
+            this.gBoxNuevoProducto.Size = new System.Drawing.Size(281, 233);
+            this.gBoxNuevoProducto.TabIndex = 13;
+            this.gBoxNuevoProducto.TabStop = false;
+            this.gBoxNuevoProducto.Text = "Nuevo Producto";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(25, 26);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(55, 20);
+            this.lblCliente.TabIndex = 14;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 15;
+            // 
+            // gBoxProductos
+            // 
+            this.gBoxProductos.Controls.Add(this.lstvProductos);
+            this.gBoxProductos.Location = new System.Drawing.Point(441, 78);
+            this.gBoxProductos.Name = "gBoxProductos";
+            this.gBoxProductos.Size = new System.Drawing.Size(267, 233);
+            this.gBoxProductos.TabIndex = 16;
+            this.gBoxProductos.TabStop = false;
+            this.gBoxProductos.Text = "Productos";
+            // 
+            // lstvProductos
+            // 
+            this.lstvProductos.Location = new System.Drawing.Point(6, 23);
+            this.lstvProductos.Name = "lstvProductos";
+            this.lstvProductos.Size = new System.Drawing.Size(248, 204);
+            this.lstvProductos.TabIndex = 0;
+            this.lstvProductos.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 564);
+            this.Controls.Add(this.gBoxProductos);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.gBoxNuevoProducto);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dtgvVender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvVender)).EndInit();
+            this.gBoxNuevoProducto.ResumeLayout(false);
+            this.gBoxNuevoProducto.PerformLayout();
+            this.gBoxProductos.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dtgvVender;
+        private Label lblCodigo;
+        private TextBox txtCodigo;
+        private TextBox txtDescripcion;
+        private Label lblDescripcion;
+        private TextBox txtCantidad;
+        private Label lblCantidad;
+        private TextBox txtPrecio;
+        private Label lblPrecio;
+        private Button btnAgregar;
+        private Button btnBorrar;
+        private DataGridViewTextBoxColumn Codigo;
+        private DataGridViewTextBoxColumn Descripcion;
+        private DataGridViewTextBoxColumn Cantidad;
+        private DataGridViewTextBoxColumn Precio;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private GroupBox gBoxNuevoProducto;
+        private Label lblCliente;
+        private TextBox textBox1;
+        private GroupBox gBoxProductos;
+        private ListView lstvProductos;
+    }
+}
